def596d972a40e4a8eb4ea58bb0f7aa685fc54b5
Hameet <hameet.singh@hotmail.com>
2018-10-28 21:22:09 +1300

    Added base_site html

--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.http import HttpResponse
 
@@ -27,6 +27,12 @@ def details(request, id):
 
 def add(request):
     if(request.method == 'POST'):
-        return
+        title = request.POST['title']
+        text = request.POST['text']
+
+        todo = Todo(title=title, text=text)
+        todo.save()
+
+        return redirect('/todos')
     else: 
         return render(request, 'add.html')
